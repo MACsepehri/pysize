a6b1d0fdeeb80b02a74edece45e8e5ee2e845f64
MACsepehri <macsepehri@gmail.com>
2026-06-13 08:57:56 +0330

    Update pysize.py

--- pysize.py
+++ pysize.py
@@ -20,7 +20,7 @@ dict_extensions = {
 
 class NoContentError(Exception): pass
 
-def get_line_by_size(file: TextIOWrapper, show_type: bool = False, linux: bool = False):
+def get_line_by_size(file: TextIOWrapper):
     try:
         file.seek(0)
         file_content = file.read()
@@ -30,27 +30,7 @@ def get_line_by_size(file: TextIOWrapper, show_type: bool = False, linux: bool =
         raise NoContentError(f"Given file doesn't have any contents (empty).")
     lines = len(file_content.splitlines())
     size = os.path.getsize(file.name)
-    if not show_type:
-        return lines
-    else:
-        if linux:
-            if size < 1024:
-                return f"{size} Byte(s)"
-            elif size < 1024 * 1024:
-                return f"{size / 1024:.2f} Kilobyte(s)"
-            elif size < 1024 * 1024 * 1024:
-                return f"{size / (1024 * 1024):.2f} Megabyte(s)"
-            else:
-                return f"{size / (1024 * 1024 * 1024):.2f} Gigabyte(s)"
-        else:
-            if size < 1000:
-                return f"{size} Byte(s)"
-            elif size < 1000 * 1000:
-                return f"{size / 1000:.2f} KB"
-            elif size < 1000 * 1000 * 1000:
-                return f"{size / (1000 * 1000):.2f} MB"
-            else:
-                return f"{size / (1000 * 1000 * 1000):.2f} GB"
+    return lines
 
 def get_size_by_charactor(file_or_char, show_type: bool = False, linux: bool = False):
     try:
